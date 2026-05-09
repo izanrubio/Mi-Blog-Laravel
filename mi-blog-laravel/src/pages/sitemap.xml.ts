05bc147719fae79866dd47e77da8b96ea386e5b5
izanrubio <izanrubiocerezo@gmail.com>
2026-05-09 18:18:06 +0200

    seo: redirect /sitemap.xml to /sitemap-index.xml

---
+++ mi-blog-laravel/src/pages/sitemap.xml.ts
@@ -0,0 +1,11 @@
+import type { APIRoute } from 'astro'
+
+export const GET: APIRoute = () => {
+  return new Response(null, {
+    status: 301,
+    headers: {
+      Location: '/sitemap-index.xml',
+      'Cache-Control': 'public, max-age=86400'
+    }
+  })
+}
